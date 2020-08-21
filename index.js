4bd0e1002e890fb1ee9b8fba24c07aed5b844774
Damilola Michael Ige <idm.calculus@gmail.com>
2020-08-21 01:59:51 +0100

    connected the app to the db

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // const functions = require('firebase-functions');
-const express = require('express');
-const cors = require('cors');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require('express');
+const cors = require('cors');
+const Note = require('./models/note');
 
 const app = express();
 
@@ -11,67 +11,35 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
-let notes = [
-	{
-	  id: 1,
-	  content: "HTML is easy",
-	  date: "2019-05-30T17:30:31.098Z",
-	  important: true
-	},
-	{
-	  id: 2,
-	  content: "Browser can execute only Javascript",
-	  date: "2019-05-30T18:39:34.091Z",
-	  important: false
-	},
-	{
-	  id: 3,
-	  content: "GET and POST are the most important methods of HTTP protocol",
-	  date: "2019-05-30T19:20:14.298Z",
-	  important: true
-	}
-];
-
 app.get('/', (req, res) => {
 	res.send(`<h1>Hello World</h1>`);
 });
 
-app.get('/api/notes', (req, res) => {
+app.get('/api/notes', async (req, res) => {
+	const notes = await Note.find({});
 	res.json(notes);
 });
 
-app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id);
-	const note = notes.find(note => note.id === id);
-	note ? response.json(note) : response.status(404).end();
+app.get('/api/notes/:id', async (request, response) => {
+	const note = await Note.findById(request.params.id);
+	response.json(note);
 });
 
-const generateId = () => {
-	const maxId = notes.length > 0
-		? Math.max(...notes.map(n => n.id))
-		: 0;
-	return maxId + 1;
-};
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', async (request, response) => {
 	const body = request.body;
-
-	if (!body.content) {
-		return response.status(400).json({ 
-		error: 'content missing' 
-		});
+  
+	if (body.content === undefined) {
+	  return response.status(400).json({ error: 'content missing' });
 	}
-
-	const note = {
-		content: body.content,
-		important: body.important || false,
-		date: new Date(),
-		id: generateId(),
-	};
-
-	notes = notes.concat(note);
-
-	response.json(note);
+  
+	const note = new Note({
+	  content: body.content,
+	  important: body.important || false,
+	  date: new Date(),
+	});
+  
+	const savedNote = await note.save();
+	response.json(savedNote);
 });
 
 app.delete('/api/notes/:id', (request, response) => {
---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const url = process.env.CONNECTION_URL;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true,
+});
+
+/* note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close();
+}); */
+
+Note.find({})
+.then(result => {
+	result.forEach(note => {
+	  console.log(note);
+	});
+	mongoose.connection.close();
+})
+.catch(err => {console.error(err)});
